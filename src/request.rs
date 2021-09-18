c9099bc64e1dd4b9f5ad7fc532872ccce46474eb
nokijp <nokijp@users.noreply.github.com>
2021-09-18 13:12:29 +0900

    add error handler

--- src/request.rs
+++ src/request.rs
@@ -17,7 +17,16 @@ pub async fn post(webhook_url: &str, request: &SimpleWebhookRequest<'_>) -> Resu
     let api_request = client.post(webhook_url)
         .header(reqwest::header::CONTENT_TYPE, "application/json")
         .body(content_json);
-    api_request.send().await.map_err(|e| format!("failed to post: {}", e))?;
+    let response = api_request.send().await.map_err(|e| format!("failed to post: {}", e))?;
+
+    let response_status = response.status();
+    if !response_status.is_success() {
+        return if let Ok(response_body) = response.text().await {
+            Err(format!("failed with {}: {}", response_status, response_body))
+        } else {
+            Err(format!("failed with {}", response_status))
+        }
+    }
 
     Ok(())
 }
